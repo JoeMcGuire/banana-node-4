90decd6bd8a1d15722a33fba80698268a30aae84
Joe McGuire <josephpaulmcguire@gmail.com>
2022-09-26 22:19:38 +0200

    putting retry back in

--- server.py
+++ server.py
@@ -5,18 +5,18 @@ import platform
 import psutil
 import multiprocessing
 import time
-#from requests.adapters import HTTPAdapter, Retry
+from requests.adapters import HTTPAdapter, Retry
 
 initStart = time.time()
 
 
-time.sleep(20)
-#retryRequest = requests.Session()
-#retries = Retry(connect=25,
-#                backoff_factor=0.2)
-#retryRequest.mount('http://', HTTPAdapter(max_retries=retries))
-#init = retryRequest.get('http://localhost:8003/init')
-init = requests.get('http://localhost:8003/init')
+#time.sleep(20)
+retryRequest = requests.Session()
+retries = Retry(connect=25,
+                backoff_factor=0.2)
+retryRequest.mount('http://', HTTPAdapter(max_retries=retries))
+init = retryRequest.get('http://localhost:8003/init')
+#init = requests.get('http://localhost:8003/init')
 # We do the model load-to-GPU step on server startup
 # so the model object is available globally for reuse
 #user_src.init()
@@ -58,7 +58,7 @@ def inference(request):
         #res = requests.get('http://localhost:8001/healthcheck') #, json = model_inputs)
     except requests.exceptions.RequestException as e:  # This is the correct syntax
         return response.json({
-            "error": "something went wrong with proxy",
+            "error": "Trying a retry again",
             "e": e,
         })
         raise SystemExit(e)
